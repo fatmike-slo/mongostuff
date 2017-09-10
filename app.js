0080ddfa25e490b8cc9286c7da78ec143506585c
Andrej <andr.stup@gmail.com>
2017-09-11 00:37:16 +0200

    exercise number 7

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let mongo = require("mongodb").MongoClient;
-let url = "mongodb://localhost:27017/"+process.argv[2];
+let url = "mongodb://localhost:27017/" + process.argv[2];
 
-mongo.connect(url, (err, db) => {
-    let collection = db.collection("users");
-    collection.update({
-        username: "tinatime"
-    }, {
-        $set: {
-            "age": 40
+mongo.connect(url, (err,db)=> {
+    if(err) {
+        console.log(err);
+    }
+    let collection = db.collection(process.argv[3]);
+    collection.remove({
+        _id:process.argv[4]
+    },(err)=> {
+        if(err) {
+            console.log(err);
         }
-    }, (err, data) => {
         db.close();
     });
-});
+})
+
